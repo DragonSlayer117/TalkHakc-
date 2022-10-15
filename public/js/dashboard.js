eabfbd1a24cc9afd279bafe8cd4a783693082d4f
luis2323 <lemunoz390@misena.edu.co>
2022-10-15 16:21:00 -0500

    cambio dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -100,14 +100,14 @@
 			new Chart(document.getElementById("chartjs-dashboard-bar"), {
 				type: "bar",
 				data: {
-					labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+					labels: ["Medicines", "Dates", "Surgeries", "Vaccines", "Tomography", "X-rays", "Controls", "Medical Exams"],
 					datasets: [{
 						label: "This year",
 						backgroundColor: window.theme.primary,
 						borderColor: window.theme.primary,
 						hoverBackgroundColor: window.theme.primary,
 						hoverBorderColor: window.theme.primary,
-						data: [54, 67, 41, 55, 62, 45, 55, 73, 60, 76, 48, 79],
+						data: [100, 67, 41, 55, 62, 45, 55, 73, 60, 76, 48, 7],
 						barPercentage: .75,
 						categoryPercentage: .5
 					}]
